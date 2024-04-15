b589b640b7069e5afe3fdb5d4835ecd358c2e22d
billelkhanfr <khanfri.billel@yahoo.fr>
2024-04-16 00:20:19 +0200

    init project

--- config/db.js
+++ config/db.js
@@ -1,11 +1,22 @@
-const mongoose = require("mongoose");
+const mysql = require("mysql");
+
 async function connectToDB() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("connected to MongoDB");
-  } catch (error) {
-    console.log("connection Failed to Mongodb", err);
-  }
+  const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error("Error connecting to MySQL: ", err.stack);
+      return;
+    }
+    console.log("Connected to MySQL as id " + connection.threadId);
+  });
+
+  connection.end();
 }
 
 module.exports = connectToDB;
